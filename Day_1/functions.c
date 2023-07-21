23e0ebf8446915fda7879f1cb48c0b9de598ca8d
preethamgouda <72795407+preethamgouda@users.noreply.github.com>
2023-07-21 23:46:57 +0530

    Add files via upload

--- Day_1/functions.c
+++ Day_1/functions.c
@@ -1,1 +1,70 @@
 
+#include<assert.h>
+#include<stdio.h>
+#include"header.h"
+
+int main()
+{
+
+    /*--------------------------------------Day 1----------------------------------*/
+
+
+   //1. Convert Upper case to Lower case.
+
+    printf("\n\n 1. Write a program that converts upper case to lower.\n");
+    printf("\n\tNote : Tested using Assertion.\n");
+    assert(lower_case('A') == 'a');
+    assert(lower_case('b') == 'b');
+    assert(lower_case(10) == INVALID);
+
+
+    //2. Calculate the area of circle.
+
+    printf("\n\n 2. Write a program to calculate the area of circle.\n");
+    float radius;
+    printf("\n\tEnter the Radius of the Circle : ");
+    scanf("%f", &radius);
+    printf("\n\tArea of the Circle             : %d \n",circle_area(radius));
+
+
+
+
+    // 6. Fahrenheit to Celsius and Celsius to Fahrenheit conversion.
+    printf("\n\n 3. Write a program to convert temperature from Celsius to Fahrenheit and \n    Fahrenheit to Celsius\n");
+    float fahrenheit, celsius;
+    printf("\n\tEnter Temperature in Fahrenheit : ");
+    scanf("%f", &fahrenheit);
+    printf("\n\tCelsius                         : %f\n",fahrenheit_to_celsius(fahrenheit));
+    printf("\n\tEnter Temperature in Celsius    : ");
+    scanf("%f", &celsius);
+    printf("\n\tFahrenheit                      : %f",celsius_to_fahrenheit(celsius));
+
+    //7. To check whether the given number is odd or even
+
+    printf("\n\n 5. Check if entered number is even or odd.\n");
+    printf("\n\tNote : Tested using Assertion.\n");
+    assert(odd_or_even(8) == EVEN);
+    assert(odd_or_even(13) == ODD);
+
+    //8. To check whether the given number is leap year or not.
+
+    printf("\n\n 6. Check if entered year is leap year or not.\n");
+    printf("\n\tNote : Tested using Assertion.\n");
+    assert(is_leap_year(2021) == FALSE);
+    assert(is_leap_year(2020) == TRUE);
+    assert(is_leap_year(2000) == TRUE);
+    assert(is_leap_year(1900) == FALSE);
+    assert(is_leap_year(-1000) == INVALID);
+
+    //9. Find power of 2^N using left shift operator.
+
+    printf("\n\n 7. WAP to find power of 2^N using left shift operator\n");
+    printf("\n\tNote : Tested using Assertion.\n");
+    assert(power_of_2(2) == 4);
+    assert(power_of_2(4) == 16);
+    assert(power_of_2(10) == 1024);
+    assert(power_of_2(-1) == INVALID); //returns INVALID if number is negative
+
+
+    return 0;
+}
--- Day_1/main.c
+++ Day_1/main.c
@@ -1,8 +1,117 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include "header.h"
+#include<math.h>
 
-int main()
+/*--------------------------------------Day 1----------------------------------*/
+
+//1. Function that converts upper case to lower
+
+char lower_case(char c)
 {
-    printf("Hello world!\n");
-    return 0;
+    if(c >='A' && c <='Z') //Using ASCII value to determine if the character is in Upper Case
+    {
+        return c = c + 32;
+    }
+    else if (c >='a' && c <='z') //Checking if character is lower case
+    {
+        return c;
+    }
+    else
+    {
+        return INVALID; //Return INVALID if the character is not valid
+    }
+
+}
+
+
+
+// 2. Function to calculate the area of circle
+
+int circle_area(float radius) //r -> Radius of a Circle
+{
+    if(radius>0){
+        return (int)PI*radius*radius; // Compute area only if radius is positive.
+    }
+    else
+    {
+        return INVALID;
+    }
+
+}
+
+
+
+
+
+// 6. Function to convert temperature from Celsius to Fahrenheit and Fahrenheit to Celsius
+
+// Fahrenheit to Celsius Conversion
+float fahrenheit_to_celsius(float f)
+{
+    float c;
+    c = (f-32)*5/9;
+    return c;
+}
+
+// Celsius to Fahrenheit Conversion
+float celsius_to_fahrenheit(float c)
+{
+    float f;
+    f = c*1.8 + 32;
+    return f;
+}
+
+
+// 7. Check if entered number is even or odd.
+
+int odd_or_even(int num) //return 2 if even, 1 if odd.
+{
+    if(num % 2 == 0)   //Checking if the number is even
+    {
+        return 2;
+    }
+    else
+    {
+       return 3;
+    }
+}
+
+
+// 8. Check if entered year is leap year or not.
+
+/* Leap year is a year which is perfectly divisible by 400,
+   or year is perfectly divisible by 4 and is not divisible
+   by 100 */
+int is_leap_year(int year) // Return 4 if year is leap year else return 3
+{
+    if (year >= 0 )  // Check if entered year is Valid
+    {
+        /*If the first operand of a logical-OR operation has a nonzero value,
+         the second operand is not evaluated.*/
+        if( (year % 400 == 0) || ((year % 4 == 0) && (year % 100 != 0)) )
+        {
+            return 1;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+    else
+    {
+        return INVALID; // Return INVALID if year is negative.
+    }
+}
+
+// 9. Find power of 2^N using left shift operator.
+
+int power_of_2(int n)
+{
+    if (n >= 0)
+    {
+        return 1 << n;
+    }
+    else
+    {
+        return INVALID; // Return INVALID if number is negative.
+    }
 }
